be770b606027607c5cead1b1e7d02e19fae25314
Taylor <bluevalkyrie37@gmail.com>
2020-04-09 21:34:56 -0400

    added some tweaks to the plots

--- creature_clusters.py
+++ creature_clusters.py
@@ -7,6 +7,7 @@ import openpyxl
 
 # sklearn libraries
 from sklearn.cluster import KMeans
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn import preprocessing
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
@@ -83,25 +84,11 @@ def create_clusters(critter_df):
 
 
 def create_final_model(X_train):
-    scaler = MinMaxScaler()
-    final_k = KMeans(n_clusters=3, init='random', n_init=10, max_iter=300, tol=1e-04, random_state=0)
+    final_k = KMeans(n_clusters=6, init='random', n_init=10, max_iter=300, tol=1e-04, random_state=0)
 
-    X = np.array(X_train.drop(['Classification'], 1).astype(float))
-    X_scaled = scaler.fit_transform(X)
-
-    y = X_train['Classification']
-    pred_y = final_k.fit(X_scaled)
-
-    # check to see how the model performed
-    correct = 0
-    for i in range(len(X)):
-        predict_me = np.array(X[i].astype(float))
-        predict_me = predict_me.reshape(-1, len(predict_me))
-        prediction = final_k.predict(predict_me)
-        if prediction[0] == y[i]:
-            correct += 1
-
-    print(correct / len(X))
+    # set initial and test sets
+    X = X_train.iloc[:, 2:4].to_numpy()
+    pred_y = final_k.fit(X)
 
     # plot the data
     plt.figure(1)
@@ -111,12 +98,20 @@ def create_final_model(X_train):
     plt.savefig('plots\\X_train.png')
 
     # plot the clusters
-    plt.scatter(X[pred_y == 0, 0], X[pred_y == 0, 1], s=50, c='green', marker='o', edgecolor='black', label='cluster 1')
+    # todo figure out how to plot my predictions
+    # plt.scatter(X[:, 0], X[:, 1], c=final_k.labels_, cmap='brg')
+    '''plt.scatter(X[pred_y == 0, 0], X[pred_y == 0, 1], s=50, c='green', marker='o', edgecolor='black', label='cluster 1')
     plt.scatter(X[pred_y == 1, 0], X[pred_y == 1, 1], s=50, c='blue', marker='o', edgecolor='black', label='cluster 2')
     plt.scatter(X[pred_y == 2, 0], X[pred_y == 2, 1], s=50, c='purple', marker='o', edgecolor='black', label='cluster 3')
+    plt.scatter(X[pred_y == 3, 0], X[pred_y == 3, 1], s=50, c='red', marker='o', edgecolor='black', label='cluster 4')'''
+
+    '''ax1.set_title('K Means')
+    plt.scatter(X[0][:, 0], X[0][:, 1], c=final_k.labels_, cmap='brg')
+    ax2.set_title("Original")
+    ax2.scatter(data[0][:, 0], data[0][:, 1], c=data[1], cmap='brg')'''
 
     # plot the centroids
-    plt.scatter(final_k.cluster_centers_[:, 0], final_k.cluster_centers_[:, 1], s=250, marker='*', c='black',
+    plt.scatter(pred_y.cluster_centers_[:, 0], pred_y.cluster_centers_[:, 1], s=250, marker='*', c='black',
                 edgecolor='black', label='centroids')
 
     plt.legend(scatterpoints=1)
@@ -125,6 +120,7 @@ def create_final_model(X_train):
 
 
 cleaned_creature_df = prepare_data(creatures_df)
+test_df = prepare_data(test_df)
 # make_plots(cleaned_creature_df)
 create_clusters(cleaned_creature_df)
 create_final_model(cleaned_creature_df)
